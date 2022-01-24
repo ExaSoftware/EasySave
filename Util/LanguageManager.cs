815a7378cbd37365da64ad309aebfd61c388b506
Lucas HIMPENS <lucas.himpens@tutamail.com>
2022-01-24 22:41:18 +0100

    #3 created language class

---
+++ Util/LanguageManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace EasySave.Util
+{
+    public static class LanguageManager
+    {
+        public static void ChangeLanguage(String language)
+        {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture(language);
+        }
+    }
+}
