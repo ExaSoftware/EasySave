20029017235d4d37bcf2b2ce4227830f22a480db
Alexandre Dupré <alexandre.dupre1@viacesi.fr>
2022-01-24 13:37:25 +0100

    Project structure

---
+++ Object/HistoryLog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EasySave
+{
+    public class HistoryLog : Log
+    {
+        private int _fileSize;
+        private string _transferTime;
+        public HistoryLog(int fileSize, string transferTime) : base(_sourceFile, _targetFile)
+        {
+            _fileSize = fileSize;
+            _transferTime = transferTime;
+        }
+
+        public int GetFileSize
+        {
+            get => this._fileSize;
+        }
+
+        public string GetTransferTime
+        {
+            get => this._transferTime;
+        }
+
+        new public bool SaveLog()
+        {
+            return _myLogModel.SaveHistoryLog(this);
+        }
+    }
+}
---
+++ Object/Log.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace EasySave
+{
+    /*
+     <summary>
+    Public Log class wich models a log
+    </summary>
+    */
+    public class Log
+    {
+
+        protected static string _time;
+        protected static string _sourceFile;
+        protected static string _targetFile;
+        protected static LogModel _myLogModel;
+        protected static View _myView;
+
+        public Log()
+        {
+
+        }
+
+        /*Log class builder*/
+        public Log(string sourceFile, string targetFile)
+        {
+            _sourceFile = sourceFile;
+            _targetFile = targetFile;
+            _time = DateTime.Now.ToString("dddd, dd MMMM yyyy HH:mm:ss");
+            _myView = new View();
+            _myLogModel = new LogModel();
+        }
+
+        public bool SaveLog()
+        {
+            return true;
+        }
+
+        public string GetTime
+        {
+            get => _time;
+        }
+
+        public string GetSourcePath
+        {
+            get => _sourceFile;
+        }
+
+        public string GetTargetFile
+        {
+            get => _targetFile;
+        }
+    }
+}
