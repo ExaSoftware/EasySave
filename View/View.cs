33f9944c61e7192d1df3644eda6cae8248c40826
Guillaume VANDEVILLE <guillaume.vandeville@viacesi.fr>
2022-01-25 14:10:27 +0100

    Update of the display

--- View/View.cs
+++ View/View.cs
@@ -6,5 +6,16 @@ namespace EasySave
 {
     public class View
     {
+        public static void Introduction(){
+            Console.WriteLine("1    Start");
+            Console.WriteLine("2    Help");
+            Console.WriteLine("3    Options");
+            Console.WriteLine("4    About the application");
+            Console.WriteLine("5    Exit");
+        }
+
+        public static void Warn() {
+            Console.WriteLine("Put a number between 1 and 5");
+        }
     }
 }
