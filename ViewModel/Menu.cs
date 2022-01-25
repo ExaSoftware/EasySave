fcf1f928f5b175de07f478e9a372551062308b73
Guillaume VANDEVILLE <guillaume.vandeville@viacesi.fr>
2022-01-25 16:05:39 +0100

    Major update of the menu's actions

--- ViewModel/Menu.cs
+++ ViewModel/Menu.cs
@@ -1,42 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using static EasySave.View;
 
 namespace EasySave
 {
-    class Menu
+    public class Menu
     {
-        public static int Start() {
-            //Menu Display
-            View.Introduction();
+        //Pick up the console input value 
+        private String _sourceDirectory = "";
+        private String _destinationDirectory = "";
+        private String _nameSave = "";
 
-            //Pick up the console input value 
+
+        public void Start() {
+            //Menu Display
+            IntroductionDisplay();
             int input = Convert.ToInt32(Console.ReadLine());
 
             //Cases of menu
             switch (input)
             {
-                //Start
+                //Start of the copy
                 case 1:
-                    return input;
-                //Help
-                case 2:
-                    return input;
-                //About the option
-                case 3:
-                    return input;
+                    ChooseSaveDisplay();
+                    //ChooseSave
+                    input = Convert.ToInt32(Console.ReadLine());
+                    switch (input)
+                    {
+                        //For Total Save
+                        case 1:
+                            Parameters();
+                            Display("Done!");
+                            break;
+                        //For Sequential Save
+                        case 2:
+                            Parameters();
+                            Display("Done!");
+                            break;
+                    }
+                    break;
                 //Option
-                case 4:
-                    return input;
-                //Language
-                case 5:
-                    return input;
-                //Something else choosen
+                case 2:
+                    ChooseLanguage();
+                    input = Convert.ToInt32(Console.ReadLine());
+                    switch (input)
+                    {
+                        //English
+                        case 1:
+                            Display("Done!");
+                            break;
+                        //French
+                        case 2:
+                            Display("Done!");
+                            break;
+                        //Warning
+                        default:
+                            Display("Put a number between 1 and 2");
+                            break;
+                    }
+                    break;
+                case 3:
+                    break;
+                //Warning
                 default:
-                    View.Warn();
+                    Display("Put a number between 1 and 3");
                     break;
             }
-            return 0;
+        }
+        //to configure the save
+        public void Parameters()
+        {
+            Display("Write the name of the source directory");
+            _sourceDirectory = Console.ReadLine();
+            Display("Write the name of the destination directory");
+            _destinationDirectory = Console.ReadLine();
+            Display("Write the name of the new save");
+            _nameSave = Console.ReadLine();
         }
     }
 }
+
