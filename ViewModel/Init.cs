ea14c5b53f10703f95ae1fda2fbda0f60cdf7451
Lucas HIMPENS <lucas.himpens@tutamail.com>
2022-01-25 13:49:51 +0100

    setup a configuration process

---
+++ ViewModel/Init.cs
@@ -0,0 +1,35 @@
+﻿using EasySave.Object;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+
+namespace EasySave.ViewModel
+{
+    public static class Init
+    {
+        /// <summary>
+        /// Load the configuration file of the application
+        /// </summary>
+        /// <returns>A Configuration Object</returns>
+        public static Configuration LoadConfiguration()
+        {
+            if (!File.Exists(Configuration.DEFAULT_CONFIG_FILE_PATH))
+            {
+                Configuration configuration = Configuration.GetInstance();
+                configuration.CreateConfigurationFile();
+                configuration.Language = Thread.CurrentThread.CurrentUICulture.Name;
+                configuration.LogsPath = @"C:\EasySave\Logs";
+                configuration.Save();
+                return configuration;
+            }
+            else
+            {
+                String jsonString = File.ReadAllText(Configuration.DEFAULT_CONFIG_FILE_PATH);
+                return JsonSerializer.Deserialize<Configuration>(jsonString);
+            }
+        }
+    }
+}
