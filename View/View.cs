8edf81a990034af0027cd73907734cfbe13f4235
Guillaume VANDEVILLE <guillaume.vandeville@viacesi.fr>
2022-01-25 16:06:12 +0100

    Major update of menu's display

--- View/View.cs
+++ View/View.cs
@@ -6,22 +6,41 @@ namespace EasySave
 {
     public class View
     {
-        //Introduction
-        public static void Introduction(){
-            Console.WriteLine("Insert one of the numbers");
-            Console.WriteLine("");
-            Console.WriteLine("1    Start");
-            Console.WriteLine("2    Help");
-            Console.WriteLine("3    Options");
-            Console.WriteLine("4    About the application");
-            Console.WriteLine("5    Exit");
-            Console.WriteLine("");
-
+        //Reusable Function
+        public static void Display(string text)
+        {
+            Console.WriteLine(text);
         }
 
-        //Warning Menu
-        public static void Warn() {
-            Console.WriteLine("Put a number between 1 and 5");
+        //Introduction
+        public static void IntroductionDisplay(){
+            Console.WriteLine("  Insert one of the numbers");
+            Console.WriteLine("");
+            Console.WriteLine("  1  -  Start");
+            Console.WriteLine("  2  -  Options");
+            Console.WriteLine("  3  -  Exit");
+            Console.WriteLine("");
+        }
+
+
+        //Display of which save to choose
+        public static void ChooseSaveDisplay()
+        {
+            Console.WriteLine("  Which save do you whish to use ?");
+            Console.WriteLine("");
+            Console.WriteLine("  1  -  Total save");
+            Console.WriteLine("  2  -  Sequential save");
+            Console.WriteLine("");
+        }
+
+        //Display of which save to choose
+        public static void ChooseLanguage()
+        {
+            Console.WriteLine("Choose the language");
+            Console.WriteLine("");
+            Console.WriteLine("  1  -  English");
+            Console.WriteLine("  2  -  French");
+            Console.WriteLine("");
         }
     }
 }
