011fa087b2cb9c12ab0e00d3d035f4fb158bbf5f
Guillaume VANDEVILLE <guillaume.vandeville@viacesi.fr>
2022-01-25 14:16:43 +0100

    Specification of the display

--- View/View.cs
+++ View/View.cs
@@ -6,14 +6,20 @@ namespace EasySave
 {
     public class View
     {
+        //Introduction
         public static void Introduction(){
+            Console.WriteLine("Insert one of the numbers");
+            Console.WriteLine("");
             Console.WriteLine("1    Start");
             Console.WriteLine("2    Help");
             Console.WriteLine("3    Options");
             Console.WriteLine("4    About the application");
             Console.WriteLine("5    Exit");
+            Console.WriteLine("");
+
         }
 
+        //Warning Menu
         public static void Warn() {
             Console.WriteLine("Put a number between 1 and 5");
         }
