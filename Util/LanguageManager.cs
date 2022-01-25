df383a5c8701d6c64b4f697585ddf193f34507d1
Lucas HIMPENS <lucas.himpens@tutamail.com>
2022-01-25 13:49:01 +0100

    #3 language manager

--- Util/LanguageManager.cs
+++ Util/LanguageManager.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using EasySave.Object;
+using System;
 using System.Globalization;
 
 namespace EasySave.Util
 {
     public static class LanguageManager
     {
-        public static void ChangeLanguage(String language)
+        public static void ChangeLanguage(String language, Configuration configuration)
         {
             CultureInfo.CurrentUICulture = new CultureInfo(language, false);
+            configuration.Language = language;
+            configuration.Save();
         }
     }
 }
