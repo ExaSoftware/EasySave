38e58d92e6e31509b994f691efb394c0e75653a7
Guillaume VANDEVILLE <guillaume.vandeville@viacesi.fr>
2022-01-26 18:00:34 +0100

    Last push of view

--- View/View.cs
+++ View/View.cs
@@ -22,6 +22,17 @@ namespace EasySave
             Console.WriteLine("");
         }
 
+        //Display of which save to choose
+        public static void DisplayChooseAction()
+        {
+            Console.WriteLine("  Choose the action");
+            Console.WriteLine("");
+            Console.WriteLine("  1  -  Create a new job backup ");
+            Console.WriteLine("  2  -  Delete a job backup");
+            Console.WriteLine("  3  -  Execute one job backup");
+            Console.WriteLine("  4  -  Execute all job backup");
+            Console.WriteLine("");
+        }
 
         //Display of which save to choose
         public static void DisplayChooseSave()
@@ -29,14 +40,14 @@ namespace EasySave
             Console.WriteLine("  Which save do you whish to use ?");
             Console.WriteLine("");
             Console.WriteLine("  1  -  Total save");
-            Console.WriteLine("  2  -  Sequential save");
+            Console.WriteLine("  2  -  Differential save");
             Console.WriteLine("");
         }
 
         //Display of which save to choose
         public static void DisplayChooseLanguage()
         {
-            Console.WriteLine("Choose the language");
+            Console.WriteLine("  Choose the language");
             Console.WriteLine("");
             Console.WriteLine("  1  -  English");
             Console.WriteLine("  2  -  French");
