354b8310521409a228001265f0cb2a6bf08d0567
Guillaume VANDEVILLE <guillaume.vandeville@viacesi.fr>
2022-01-27 12:39:02 +0100

    Feature CheckInput and typo fixes

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,174 +1,219 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using static EasySave.View;
 
 namespace EasySave
 {
     public class MainViewModel
     {
         //Pick up the console input value 
-        private List <JobBackup> listOfJobBackup = new List<JobBackup>(5);
+        private List<JobBackup> listOfJobBackup = Init.CreateJobBackupList();
+        View view = new View();
 
-        public void Start() {
-            //Menu Display
-            DisplayIntroduction();
-            int input = Convert.ToInt32(Console.ReadLine());
-            //Cases of menu
-            switch (input)
-            {
-                //Start
-                case 1:
-                    DisplayChooseAction();
-                    input = Convert.ToInt32(Console.ReadLine());
-                    switch (input)
-                    {
-                        //Create a new job backup
-                        case 1:
-                            //Check if the number of saves is inferior or equal to 5
-
-                            DisplayChooseSave();
-                            input = Convert.ToInt32(Console.ReadLine());
-                            //Choose if you want a total or sequential save
-                            switch (input)
-                            {
-                                //For Total Save
-                                case 1:
-                                    //Insert in the list the job
-                                    Display("  Choose a space from 1 to 5");
-                                    DisplayJobBackup();
-                                    input = Convert.ToInt32(Console.ReadLine())-1;
-                                    if (input > 5)
-                                    {
-                                        Display("  Not a choice, restarting");
-                                        Start();
-
-                                    }
-                                    Display("  Write the name of the source directory");
-                                    listOfJobBackup[input].Label = Console.ReadLine();
-                                    Display("  Write the name of the source directory");
-                                    listOfJobBackup[input].SourceDirectory = Console.ReadLine();
-                                    Display("  Write the name of the destination directory");
-                                    listOfJobBackup[input].DestinationDirectory = Console.ReadLine();
-                                    listOfJobBackup[input].IsDifferential = false;
-                                    Display("  Created!");
-                                    Display("");
-                                    Start();
-                                    break;
-
-                                //For Differential Save
-                                case 2:
-                                    //Insert in the list the job
-                                    Display("  Veuillez choisir un emplacement");
-                                    input = Convert.ToInt32(Console.ReadLine());
-                                    if (input > 5)
-                                    {
-                                        Display("  Not a choice, restarting");
-                                        Start();
-                                    }
-                                    Display("Write the name of the new save");
-                                    listOfJobBackup[input].Label = Console.ReadLine();
-                                    Display("  Write the name of the source directory");
-                                    listOfJobBackup[input].SourceDirectory = Console.ReadLine();
-                                    Display("  Write the name of the destination directory");
-                                    listOfJobBackup[input].DestinationDirectory = Console.ReadLine();
-                                    listOfJobBackup[input].IsDifferential = true;
-                                    Console.Clear();
-                                    Display("  Created!");
-                                    Display("");
-                                    Start();
-                                    break;
-                            }
-                            break;
-                        //To delete a job backup
-                        case 2:
-                            DeleteSave();
-                            Display("  Deleted!");
-                            Start();
-                            break;
-                        //Execute one job backup
-                        case 3:
-                            Display("  Veuillez choisir un job");
-                            DisplayJobBackup();
-                            input = Convert.ToInt32(Console.ReadLine());
-                            listOfJobBackup[input].Execute();
-                            Display("  Done!");
-                            Start();            
-                            break;
-
-                        //Execute all jobs backup
-                        case 4:
-                            foreach (JobBackup item in listOfJobBackup)
-                            {
-                                item.Execute();
-                            }
-                            Display("  Done!");
-                            Start();
-                            break;
-                        //Warn
-                        default:
-                            Display("  Put a number between 1 and 4");
-                            break;
-                    }
-                    break;
-                   //Option
-                case 2:
-                    DisplayChooseLanguage();
-                    input = Convert.ToInt32(Console.ReadLine());
-                    switch (input)
-                    {
-                        //English
-                        case 1:
-                            Display("  Done!");
-                            break;
-                        //French
-                        case 2:
-                            Display("  Done!");
-                            break;
-                        //Warning
-                        default:
-                            Display("  Put a number between 1 and 2");
-                            break;
-                    }
-                    break;
-                case 3:
-                    break;
-                //Warning
-                default:
-                    Display("  Put a number between 1 and 3");
-                    break;
-            }
-        }
-        //to delete a job backup
-        private void DeleteSave()
+        public void Start()
         {
-            Display("  Which save do you whish to delete ?");
-            DisplayJobBackup();
-            int input = Convert.ToInt32(Console.ReadLine())-1;
-
-            //Choose the job backup in the list
-            listOfJobBackup[input].Reset();
-
-        }
-        private void DisplayJobBackup()
-        {
-            int max = 1;
-            foreach (JobBackup i in listOfJobBackup)
+            Boolean app = true;
+            while (app is true)
             {
-                String type = "";
-                if(i.IsDifferential is true && i.Label != null)
+                view.DisplayIntroduction();
+                //Menu Display
+                int input = CheckInput();
+                //Cases of menu
+                switch (input)
                 {
-                    type = "Differential";
-                    Display("  " + max + "  - Label: " + i.Label + " SourceDirectory: " + i.SourceDirectory + " DestinationDirectory:" + i.DestinationDirectory + " Type:" + type);
-                    max++;
-                }
-                else if (i.IsDifferential is false && i.Label !=null)
-                {
-                    type = "Total";
-                    Display("  " + max + "  - Label: " + i.Label + " SourceDirectory: " + i.SourceDirectory + " DestinationDirectory:" + i.DestinationDirectory + " Type:" + type);
-                    max++;
+                    //Start
+                    case 1:
+                        view.DisplayChooseAction();
+                        input = CheckInput();
+                        switch (input)
+                        {
+                            //Create a new job backup
+                            case 1:
+                                //Check if the number of saves is inferior or equal to 5
+                                view.DisplayChooseSave();
+                                input = CheckInput();
+                                //Choose if you want a total or sequential save
+                                switch (input)
+                                {
+                                    //For Total Save
+                                    case 1:
+                                        Console.Clear();
+                                        //Insert in the list the job
+                                        view.Display("");
+                                        view.Display("  Choose a space from 1 to 5");
+                                        DisplayJobBackup();
+                                        input = CheckInput() - 1;
+                                        while (input > 5)
+                                        {
+                                            view.Display("  Not a choice, restarting");
+                                            input = CheckInput() -1;
+
+                                        }
+                                        view.Display("  Write the name of the source directory");
+                                        listOfJobBackup[input].Label = Console.ReadLine();
+                                        view.Display("  Write the name of the source directory");
+                                        listOfJobBackup[input].SourceDirectory = Console.ReadLine();
+                                        view.Display("  Write the name of the destination directory");
+                                        listOfJobBackup[input].DestinationDirectory = Console.ReadLine();
+                                        listOfJobBackup[input].IsDifferential = false;
+                                        Console.Clear();
+                                        view.Display("  Created!");
+                                        view.Display("");
+                                        break;
+
+                                    //For Differential Save
+                                    case 2:
+                                        Console.Clear();
+                                        //Insert in the list the job
+                                        view.Display("  Veuillez choisir un emplacement");
+                                        input = CheckInput() -1;
+                                        while (input > 5)
+                                        {
+                                            view.Display("  Not a choice, restarting");
+                                            input = CheckInput() -1;
+
+                                        }
+                                        view.Display("Write the name of the new save");
+                                        listOfJobBackup[input].Label = Console.ReadLine();
+                                        view.Display("  Write the name of the source directory");
+                                        listOfJobBackup[input].SourceDirectory = Console.ReadLine();
+                                        view.Display("  Write the name of the destination directory");
+                                        listOfJobBackup[input].DestinationDirectory = Console.ReadLine();
+                                        listOfJobBackup[input].IsDifferential = true;
+                                        Console.Clear();
+                                        view.Display("  Created!");
+                                        view.Display("");
+                                        break;
+                                    //Return to the start
+                                    case 3:
+                                        break;
+                                      
+                                }
+                                break;
+                            //To delete a job backup
+                            case 2:
+                                DeleteSave();
+                                Console.Clear();
+                                view.Display("  Deleted!");
+                                break;
+                            //Execute one job backup
+                            case 3:
+                                view.Display("  Veuillez choisir un job");
+                                DisplayJobBackup();
+                                input = CheckInput() - 1;
+                                listOfJobBackup[input].Execute();
+                                Console.Clear();
+                                view.Display("  Done!");
+                                break;
+
+                            //Execute all jobs backup
+                            case 4:
+                                foreach (JobBackup item in listOfJobBackup)
+                                {
+                                    item.Execute();
+                                }
+                                Console.Clear();
+                                view.Display("  Done!");
+                                break;
+                            case 5:
+                                break;
+                            //Warn
+                            default:
+                                view.Display("  Put a number between 1 and 5");
+                                input = CheckInput();
+                                break;
+                        }
+                        break;
+                    //Option
+                    case 2:
+                        view.DisplayChooseLanguage();
+                        input = CheckInput();
+                        switch (input)
+                        {
+                            //English
+                            case 1:
+                                Console.Clear();
+                                view.Display("  Done!");
+                                break;
+                            //French
+                            case 2:
+                                Console.Clear();
+                                view.Display("  Done!");
+                                break;
+                            //Warning
+                            default:
+                                view.Display("  Put a number between 1 and 2");
+                                input = CheckInput();
+                                break;
+                        }
+                        break;
+                    case 3:
+                        app = false;
+                        break;
+                    //Warning
+                    default:
+                        view.Display("  Put a number between 1 and 3");
+                        input = CheckInput();
+                        break;
                 }
             }
+        }
+            //to delete a job backup
+            private void DeleteSave()
+            {
+                view.Display("  Which save do you whish to delete ?");
+                DisplayJobBackup();
+                view.Display("  Other  - Return to start");
+                int input = CheckInput() - 1;
+                switch (input)
+                {
+                    default:
+                        Start();
+                        break;
+                }
+                //Choose the job backup in the list
+                listOfJobBackup[input].Reset();
+            }
+            private void DisplayJobBackup()
+            {
+                int max = 1;
+                foreach (JobBackup i in listOfJobBackup)
+                {
+                    String type = "";
+                    if (i.IsDifferential is true && i.Label != "")
+                    {
+                        type = "Differential";
+                        view.Display("  " + max + "  - Label: " + i.Label + " SourceDirectory: " + i.SourceDirectory + " DestinationDirectory:" + i.DestinationDirectory + " Type:" + type);
+                        max++;
+                    }
+                    else if (i.IsDifferential is false && i.Label != "")
+                    {
+                        type = "Total";
+                        view.Display("  " + max + "  - Label: " + i.Label + " SourceDirectory: " + i.SourceDirectory + " DestinationDirectory:" + i.DestinationDirectory + " Type:" + type);
+                        max++;
+                    }
+                }
+            }
+        private int CheckInput()
+        {
+            Boolean test = false;
+            String letter = "";
+
+            while (test != true)
+            {
+                letter = Console.ReadLine();
+                try
+                {
+                    int saisie = int.Parse(letter) ;
+                    test = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("  Please put a number");
+                }   
+                
+            }
+            return Convert.ToInt32(letter);
         }
     }
 }
