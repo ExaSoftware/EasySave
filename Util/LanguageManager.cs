89a74553d6f9ecdf15b3a890f16bfc1fcc8f7983
Lucas HIMPENS <lucas.himpens@tutamail.com>
2022-01-28 14:52:31 +0100

    #6 added comments

--- Util/LanguageManager.cs
+++ Util/LanguageManager.cs
@@ -6,6 +6,11 @@ namespace EasySave.Util
 {
     public static class LanguageManager
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="language">The want to setup</param>
+        /// <param name="configuration">The configuration object</param>
         public static void ChangeLanguage(String language, Configuration configuration)
         {
             CultureInfo.CurrentUICulture = new CultureInfo(language, false);
