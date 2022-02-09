a235b3724c89b3559c04345c9472e4e921d0cbf5
Lucas HIMPENS <lucas.himpens@tutamail.com>
2022-02-09 21:20:38 +0100

    Move data processing display for extensions and language into the view model + return to main view when clicked on validate

--- ViewModel/SettingsViewModel.cs
+++ ViewModel/SettingsViewModel.cs
@@ -8,10 +8,28 @@ namespace EasySave.ViewModel
     class SettingsViewModel
     {
         private Configuration _configuration;
+        private int _selectedLanguage;
+        private String _extensions;
 
         public SettingsViewModel()
         {
             _configuration = Init.LoadConfiguration();
+
+            //Set the attributes to set the selected language in the combobox
+            if (String.Equals(_configuration.Language, "fr-FR"))
+            {
+                _selectedLanguage = 0;
+            }
+            if (String.Equals(_configuration.Language, "en-US"))
+            {
+                _selectedLanguage = 1;
+            }
+
+            //Set the attributes to the convert the list into a string fot the display in the field in view
+            if (_configuration.Extensions != null)
+            {
+                _extensions = String.Join(";", _configuration.Extensions);
+            }
         }
 
         public void SaveSettings(String language, String businessSoftware, String[] extensions)
@@ -24,5 +42,7 @@ namespace EasySave.ViewModel
         }
 
         public Configuration Configuration { get => _configuration; set => _configuration = value; }
+        public int SelectedLanguage { get => _selectedLanguage; set => _selectedLanguage = value; }
+        public String Extensions { get => _extensions; set => _extensions = value; }
     }
 }
