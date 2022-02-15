48adedfdf72036c512050816bd193e657a1ff671
Guillaume VANDEVILLE <guillaume.vandeville@viacesi.fr>
2022-02-15 16:47:27 +0100

    first commit to check the input in setting'

--- ViewModel/SettingsViewModel.cs
+++ ViewModel/SettingsViewModel.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Reflection;
+using System.Resources;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
 
 namespace EasySave.ViewModel
 {
@@ -10,6 +14,8 @@ namespace EasySave.ViewModel
         private int _selectedLanguage;
         private String _extensions;
         private String _businessSoftware;
+        private ResourceManager _rm = new ResourceManager("EasySave.Resources.Strings", Assembly.GetExecutingAssembly());
+        private Dictionary<string, string> _errors = new Dictionary<string, string>();
 
         public SettingsViewModel()
         {
@@ -40,8 +46,21 @@ namespace EasySave.ViewModel
             App.Configuration.Extensions = extensions;
             App.Configuration.Save();
         }
+        public void CheckExtension(string extensions)
+        {
+            string RegEx = @"[^\w^;^.]+";
+            string RegEx2 = @"(\.[a-zA-Z1-9]{0,20};*)";
+            Match result = Regex.Match(extensions, RegEx);
+            MessageBox.Show(result.Success.ToString());
+            if (result.Success)
+            {
+                _errors.Add("destinationDirectoryError", _rm.GetString("emptyDestinationDirectoryError"));
+            }
+        }
         public int SelectedLanguage { get => _selectedLanguage; set => _selectedLanguage = value; }
         public String Extensions { get => _extensions; set => _extensions = value; }
         public string BusinessSoftware { get => _businessSoftware; set => _businessSoftware = value; }
+        public Dictionary<string, string> Errors { get => _errors; set => _errors = value; }
+
     }
 }
